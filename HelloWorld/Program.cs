753d136af2687e7b6309a15f3a7425c4b80570e4
prabhushankar07 <prabhushankar3369@gmail.com>
2026-02-23 15:06:30 +0530

    dotnet basics

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld // Matches your folder name
+{
+    class Program // Standard naming, matches your Program.cs file
+    {
+        public static void Main(string[] args)
+        {
+            // Example usage
+            Console.WriteLine("Factorial of 5: ");
+            Console.WriteLine("Is 7 Prime? ");
+        }
+    }
+}
